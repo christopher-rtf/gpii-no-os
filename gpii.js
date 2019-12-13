b92f28d22b1e637f9ef2e142d7db1d17453c1bab
Christopher - RtF <58520035+christopher-rtf@users.noreply.github.com>
2019-12-12 21:50:41 -0400

    GPII-4253: Created initial gpii stubs for non-supported OSs

---
+++ gpii.js
@@ -0,0 +1,29 @@
+/*
+ * GPII "No OS" Personalization Framework Node.js Bootstrap
+ *
+ * Copyright 2019 Raising the Floor -- US Inc. All rights reserved.
+ * Copyright 2012 OCAD University
+ *
+ * Licensed under the New BSD license. You may not use this file except in
+ * compliance with this License.
+ *
+ * You may obtain a copy of the License at
+ * https://github.com/GPII/universal/blob/master/LICENSE.txt
+ * 
+ * The R&D leading to these results received funding from the
+ * Department of Education - Grant H421A150005 (GPII-APCP). However,
+ * these results do not necessarily represent the policy of the
+ * Department of Education, and you should not assume endorsement by the
+ * Federal Government.
+ *
+ * The research leading to these results has received funding from the European Union's
+ * Seventh Framework Programme (FP7/2007-2013)
+ * under grant agreement no. 289016.
+ */
+
+"use strict";
+
+var fluid = require("gpii-universal"),
+    gpii = fluid.registerNamespace("gpii");
+
+gpii.start();
